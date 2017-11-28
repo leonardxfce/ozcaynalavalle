85ae3a49fe488e12ae88e55a5f8679d3e8a09fae
leonardxfce <leonardo.araoz.dev@gmail.com>
2017-11-27 21:53:51 -0300

    cambios de errores

--- js/funciones.js
+++ js/funciones.js
@@ -1,28 +1,28 @@
-  $(function(){
-    $("#slides").slidesjs({
-      play: {
-        active: true,
-        effect: "slide",
-        interval: 5000,
-        auto: true,
-        swap: true,
-        pauseOnHover: false,
-        restartDelay: 500
-      }
-    });
+/*global $*/
+$(function () {
+  $("#slides").slidesjs({
+    play: {
+      active: true,
+      effect: "slide",
+      interval: 5000,
+      auto: true,
+      swap: true,
+      pauseOnHover: false,
+      restartDelay: 500
+    }
   });
-  $(function() {
+});
+$(function () {
 
-   var btn_movil = $('#nav-responsive'),
-   menu = $('#navegacion').find('ul');
+  var btn_movil = $('#nav-responsive'),
+    menu = $('#navegacion').find('ul');
 
-        // Al dar click agregar/quitar clases que permiten el despliegue del menú
-        btn_movil.on('click', function (e) {
-          e.preventDefault();
+  btn_movil.on('click', function (e) {
+    e.preventDefault();
 
-          var el = $(this);
+    var el = $(this);
 
-          el.toggleClass('nav-active');
-          menu.toggleClass('open-menu');  
-        });
-      });
+    el.toggleClass('nav-active');
+    menu.toggleClass('open-menu');
+  });
+});
